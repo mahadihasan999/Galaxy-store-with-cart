9b220e15e4210da155510f2be2f89f4923a325f9
mahadihasan999 <mahadihasan445541@gmail.com>
2021-09-14 14:54:15 +0600

    Everything is ok

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// fetch url api
 const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
@@ -13,19 +14,56 @@ const showProducts = (products) => {
     const image = product.images;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `<div class="">
+    <div class="single-product bg-light h-50">
       <div>
-    <img class="product-image" src=${image}></img>
+          <img class="product-image" src=${product.image}></img>
       </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <h4 >${product.title.slice(0, 14)}</h4>
+      <p>Category:${product.category}</p>
+      <p> <i class="fas fa-star filled"></i> Rating: ${product.rating.rate} (${product.rating.count})</p>
+      <h4 class="text-danger">Price: $ ${product.price}</h4>
+
+       <button onclick="addToCart(${product.id},${product.price})" class="btn" id="addToCart-btn">Add to cart</button> 
+
+       <button onclick="loadStoreDetails(${product.id})" id="details-btn" class="btn btn-danger">Details</button> 
+
+      </div>
+      </div>
       `;
+    //append in div
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+//Show detail  as a single product in UI
+const loadStoreDetails = storeId => {
+  console.log(storeId);
+  const url = `https://fakestoreapi.com/products/${storeId}`;
+  console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayStoreDetail(data))
+}
+
+const displayStoreDetail = store => {
+  const storeDetails = document.getElementById('product-detail');
+  storeDetails.textContent = '';
+  const div = document.createElement('div');
+  div.classList.add('card');
+  console.log(store);
+  div.innerHTML = `
+  <div class="detail-store">
+  <div class="p-2 ">
+      <h5 class="text-danger">Details: ${store.title.slice(0, 25)}</h5>
+      <p>${store.description.slice(0, 100)}</p>
+  </div>
+  
+</div>`;
+  storeDetails.appendChild(div);
+}
+
+//Product count
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -33,11 +71,14 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  displayStoreDetail('none')
+
 };
 
+// set innerText in id
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -69,12 +110,15 @@ const updateTaxAndCharge = () => {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
+  updateTotal()
 };
 
 //grandTotal update function
 const updateTotal = () => {
-  const grandTotal =
-    getInputValue("price") + getInputValue("delivery-charge") +
+  const grandTotal = getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+
+  // toFixed 2 decimal number
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
